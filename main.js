9a0161a19f40c7a3e7271c4f6d480cfb85ea7c91
Seungyeop Baek <yup6786@naver.com>
2024-06-20 03:01:13 +0900

    maybe final commit

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 var fs = require('fs');
+var path = require('path');
 
 app.listen(3000, function(){
     console.log('Listening on port 3000');
@@ -11,7 +12,7 @@ app.listen(3000, function(){
 // Jade Template Engine의 소스를 깔끔하게 보기 위한 코드.
 app.locals.pretty = true;
 app.set('view engine', 'jade');
-app.set('views', './doosan');
+app.set('views', path.join(__dirname, './doosan'));
 
 app.get('/', (req ,res)=>{
     res.redirect('/main');
@@ -19,6 +20,9 @@ app.get('/', (req ,res)=>{
 
 app.get('/main', (req, res) => {
     var backnumber = req.query.backnumber; // 쿼리 파라미터로 backnumber 가져오기
+    var coaches = JSON.parse(fs.readFileSync("./players/coaches.json", "utf-8"));
+    var pitchers = JSON.parse(fs.readFileSync("./players/pitchers.json", "utf-8"));
+    var batters = JSON.parse(fs.readFileSync("./players/batters.json", "utf-8"));
 
     if (backnumber) {
         fs.readFile('players/' + backnumber, 'utf-8', function(err, data) {
@@ -30,7 +34,7 @@ app.get('/main', (req, res) => {
             }
         });
     } else {
-        res.render('search');
+        res.render('search', { coaches: coaches, pitchers: pitchers, batters: batters });
     }
 });
 
